55d3556dc5f7ce42cb68179112d78ce9b946574d
luisangelquinones <luisangelq3673@gmail.com>
2020-12-26 00:38:40 -0600

    Added redux configuration and server as json-server

---
+++ src/components/Products.js
@@ -0,0 +1,24 @@
+import { Fragment } from 'react'
+
+const Products = () => {
+    return(
+        <Fragment>
+            <h2 className="text-center my-5">Products list</h2>
+
+            <table className="table table-striped">
+                <thead className="bg-primary table-dark">
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </Fragment>
+    )
+}
+
+export default Products;
